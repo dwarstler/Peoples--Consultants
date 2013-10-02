fe9de07f11f38d58f85953de02757c584a8d0ac7
Doug Warstler <dougie.warstler21@gmail.com>
2013-10-02 15:46:30 -0400

    start again

---
+++ PeoplesC/app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :name, :password, :is_admin, :created_at, :updated_at
---
+++ PeoplesC/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PeoplesC::Application.config.secret_key_base = '6c291efaa8c2d9796cc32206d4327f1f15d26e840741ac775d061994fb0c4a89cd34380df807e1a9ee94841771bbefcb027f6e8cf730ca915dc5db4a5d0a2d28'
