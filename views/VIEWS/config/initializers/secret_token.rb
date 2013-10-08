9bdf255b64dc94ee78cd899b1b64aed482549ee7
blakeyoder <yoderblake@gmail.com>
2013-10-07 22:05:48 -0400

    with new carousel

---
+++ views/VIEWS/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VIEWS::Application.config.secret_key_base = '6416e7f0fcff7644965220ba0ace8b46b732385873b601aba1ac942a2f095a917d7213d033a9fe64303a424dff3eb62fa274a4f7332b3ed3739914d9c937bf59'
